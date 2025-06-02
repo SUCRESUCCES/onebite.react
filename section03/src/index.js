091758e949ebf5ebd31762a2ed35ea6b149e2b6a
밍 <sucresucces@icloud.com>
2025-06-02 16:32:50 +0900

    라이브러리란? 프로그램을 개발할 때 필요한 다양한 기능들을 미리 만들어 모듈화 해 놓은 것
    node_modules/: 설치된 라이브러리 파일들이 저장됨
    
    package-lock.json: 정확한 버전 정보를 기록
    
    package.json: 설치된 라이브러리 이름과 버전 정보 기록됨
    
    문제 상황
    node_modules/와 package-lock.json 삭제 시, 실행하면 오류 발생 (모듈을 찾을 수 없음)
    
    ✅ 해결 방법
    package.json만 남아 있다면 터미널에 다음 명령어 입력하여 복구 가능:
    npm install

--- section03/src/index.js
+++ section03/src/index.js
@@ -11,6 +11,14 @@
 // import mul from "./math.js";
 // import { add, sub } from "./math.js";
 import mul, { add, sub } from "./math.js";
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+// node.modules의 randomColor라이브러리파일과 package-lock.json이 삭제됐을 경우 오류가 발생함
+// package.json에 정보만 있어도 터미널에 npm i || npm install하면 다시 설치됨
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
