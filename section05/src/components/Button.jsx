f0a7fd14afeb84a0efd84f8d12216e412694cd61
밍 <sucresucces@icloud.com>
2025-06-17 15:45:55 +0900

    이벤트 핸들링 : 웹 내부에서 발생하는 사용자의 행동
    ex) 버튼 클릭시 경고장 노출
    
    합성이벤트(SyntheticBaseEvent)
    : 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태
    
    Event객체가 서로 다름
    
    이벤트 함수 전달 방식
    <button onClick={onClickButton}>클릭</button>
    - onClick={onClickButton} : 함수의 이름만 전달!!
    - 함수 이름 뒤에 ()를 붙이면 컴포넌트가 렌더링될 때 즉시 실행되므로 절대 하지 않아야함!

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,12 +1,24 @@
 // props의 기본값 설정할 때
 // : 구조분해할당으로 기본값 받기
 const Button = ({ children, text, color = "black" }) => {
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton} // 함수의 이름만 전달
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
   );
 };
 
+Button.defaultProps = {
+  color: "black",
+};
 export default Button;
