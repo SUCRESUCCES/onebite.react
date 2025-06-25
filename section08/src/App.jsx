4cda4d1593b6358d032a9e2d962fb340bf059b7d
밍 <sucresucces@icloud.com>
2025-06-25 22:14:29 +0900

    TodoList - Create(투두 추가하기)
    idRef.current++
    - 고유한 id 생성
    
    onKeyDown
    - 키 눌렀을 때 발생되는 이벤트
    
    onSubmit()
    - 입력값이 비어있으면 포커스만 줌
    -  내용이 있으면 onCreate(content) 실행 -> 투두 추가
    - 입력창 비우기 (setContent("")
    
    onCreate(content)
    - 새로운 todo 객체 생성
    - setTodos([newTodo, ...todos]) 로 리스트에 추가
    
    content 상태
    - 입력창에 현재 입력 중인 텍스트를 관리
    
    onChangeContent()
    - input에 텍스트 입력할 때마다 content 상태 업데이트

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -27,10 +27,22 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++, // 고유한 id 생성
+      isDone: false,
+      content: content, // 사용자가 입력한 내용
+      date: new Date().getTime(), // 현재 시간
+    };
+
+    setTodos([newTodo, ...todos]); // 새로운 todo를 기존 배열 앞에 추가
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
