544ead85ad9dc0209136ac948afb16a6e86d7f2c
밍 <sucresucces@icloud.com>
2025-06-27 12:22:46 +0900

    useReducer
    ❕ 주요 개념
    useReducer
    - 컴포넌트 내부에 새로운 State를 생성하는 React Hook
    - 모든 useState는 useReducer로 대체 가능
    
    useState를 사용하면?
    - 컴포넌트 내부에 상태 관리 코드를 작성해야 함
    
    useReducer를 사용하면?
    - 컴포넌트 외부에 상태 관리 코드를 분리할 수 있음
    
    - 상태 업데이트 로직을 컴포넌트 외부의 reducer 함수로 분리해 더 명확하고 구조적인 코드 작성이 가능
    
    ❕ 구성 요소
    - reducer(state, action) : 상태를 어떻게 변경할지 정의하는 함수
    - dispatch(action) : 상태 변경 요청을 발생시키는 함수
    - action 객체 : {type: "INCREASE", data : 1} 형태로, 어떤 종류의 상태 변경인지 명시
    
    ❕ 사용 이유 및 장점
    - 복잡한 상태 로직을 외부로 분리해 코드 가독성 및 재사용성 향상
    - 상태 변화가 모두 dispatch를 통해 일어나서 흐름이 명확하고 예측 가능
    - 여러 상태가 얽힌 경우 useState보다 관리하기 쉬움
    - 나중에 전역 상태 관리 도구 (Reduc 등)와 개념적으로 연결됨
    
    ❕ 요약 예시 동작
    버튼 클릭 → dispatch({ type: "INCREASE", data: 1 }) 실행 → 상태 +1
    
    버튼 클릭 → dispatch({ type: "DECREASE", data: 1 }) 실행 → 상태 -1
    
    💥 기타
    default case에 return statem 빠지면 undefined 반환 주의!

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,40 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // Enter키 누르면 자동으로 onSubmit()이 실행되서 엔터만으로도 todo 추가 가능
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus(); // 입력값이 없을 때 포커스
+      return;
+    }
+    onCreate(content); // App에서 전달된 함수 호출
+    setContent(""); // 입력창 초기화
+  };
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown} // 사용자가 키보드를 누를 때 발생하는 이벤트
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
