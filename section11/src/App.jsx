b6625feccf47b2ae69ad35c19a0954e45cde5763
밍 <sucresucces@icloud.com>
2025-07-01 14:03:40 +0900

    Context 분리하기
    왜 문제가 발생했을까?
    - value에 객체를 props로 넘기면 렌더링 될 때마다 새로운 객체로 간주돼서, 하위 컴포넌트들도 불필요하게 렌더링
    
    어떻게 해결할 수 있을까?
    - 두 개의 컨텍스트로 분리해서 해결
    ( 변경될 수 있는 값 : StateContext / 변경되지 않는 값 : DispatchContext )
    
    App.jsx에서  useMemo 사용하는 이유?
    - onCreate, onUpdate, onDelete는 렌더될 때 마다 바뀌지 않기 때문에
    - useMemo로 감싸면 해당 객체가 변하지 않아서 불필요한 리렌더링 방지
    
    List 컴포넌트에서 구조분해할당을 하지않는 이유?
    const todos = useContext(TodoStateContext);
    - useContext(TodoContext)는 배열(todos)자체이므로, 구조분해 할당이 아닌 그대로 변수에 담아야함
    - 구조분해할당은 객체일 때 쓰는 방식이기 때문에 여기서는 사용하지 않음
    
    ✔ 결과
    - 불필요한 리렌더링 방지
    - 상태와 액션을 명확히 구분하여 관리 용이

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -5,6 +5,7 @@ import {
   useReducer,
   useCallback,
   createContext,
+  useMemo,
 } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
@@ -48,7 +49,8 @@ function reducer(state, action) {
   }
 }
 
-export const TodoContext = createContext();
+export const TodoStateContext = createContext();
+export const TodoDispatchContext = createContext();
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
@@ -80,21 +82,20 @@ function App() {
     });
   }, []);
 
+  const memoizedDispatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  }, []);
+
   return (
     <div className="App">
       {/* <Exam /> */}
       <Header />
-      <TodoContext.Provider
-        value={{
-          todos,
-          onCreate,
-          onUpdate,
-          onDelete,
-        }}
-      >
-        <Editor />
-        <List />
-      </TodoContext.Provider>
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider value={memoizedDispatch}>
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
