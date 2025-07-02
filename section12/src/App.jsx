cea1ae19f9521a62c3bc761bb3255f7be2e5ac11
밍 <sucresucces@icloud.com>
2025-07-02 21:53:55 +0900

    리액트 이미지 관리 & 빌드 최적화 요약 정리
    ✔ 1. 빌드 관련
    - npm run build : 최적화된 결과물 생성 -> dist 폴더에 저장
    - npm run preview : 로컬 서버에서 빌드 결과물 미리보기
    
    ✔ 2. 이미지 관리 방식
    | 구분     | `public` 폴더                   | `src/assets` 폴더 (`import`)                      |
    | ------ | ----------------------------- | ----------------------------------------------- |
    | 경로     | `/emotion1.png`처럼 경로 직접 사용    | `import emotion1 from "../assets/emotion1.png"` |
    | 최적화    | ❌ 자동 최적화 X                    | ✅ 빌드시 자동 최적화 (압축, 변환, 캐싱 등)                     |
    | 사용 예   | `<img src="/emotion1.png" />` | `<img src={emotion1} />`                        |
    | 적합한 상황 | 이미지가 많고 동적으로 처리해야 할 때         | 이미지 수가 적고 정적으로 사용될 때                            |
    
    💥 주의
    - 이미지가 많고 관리가 복잡한 경우는 public 폴더 사용이 나을 수 있음
    - 이미지가 소수이고 컴포넌트 내에서 사용할 경우는 assets + import 방식이 더 좋음
    
    ✔ 3.캐싱과 Data URI
    - import된 이미지들은 빌드 시 Data URI 포맷이나 해시 기반 경로로 변환됨.
    - 브라우저는 한 번 불러온 이미지들을 메모리에 캐싱함.
    - Networt 탭에서 0ms 로딩시간으로 나타나면 -> 메모리 캐시에서 바로 가져온 것.
    
    ✔ 4. 로그 보존 (Network 탭)
    - Preserve log 체크 시:
    	- 새로고침해도 이전 네트워크 요청 로그가 유지됨
    	- 캐시 여부를 확인할 때 유용함

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,8 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 
+import { getEmotionImage } from "./util/get-emotion-image";
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
@@ -16,6 +18,23 @@ function App() {
   };
   return (
     <>
+      {/* public 폴더에 넣은 것
+      이미지 최적화 하지않을거면 이미지를 public에 넣어도 상관 없음! */}
+      {/* <div>
+        <img src={"/emotion1.png"} />
+        <img src={"/emotion2.png"} />
+        <img src={"/emotion3.png"} />
+        <img src={"/emotion4.png"} />
+        <img src={"/emotion5.png"} />
+      </div> */}
+      {/* import로 src의 assets에서 가져온 것! */}
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
