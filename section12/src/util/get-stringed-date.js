1661149379ba72c56a49cfabd1405863b9f82128
밍 <sucresucces@icloud.com>
2025-07-09 21:48:52 +0900

    Diary 페이지 구현하기
    1. emotionList와 getStringedDate를 util로 분리
    = 재사용성과 코드 정리
    
    - 각 컴포넌트에서 직접 정의하면 중복되기 쉽고,
    - 다른 컴포넌트에서 공유하기 불편함
    
    2.useDiary를 커스텀 훅으로 만든 이유
    = 중복 로직 추출 + 상태 관리 캡슐화
    - 로직 재사용
    (일기 조회 로직을 여러 곳에서 동일하게 사용 가능)
    - 코드 간결화
    (컴포넌트에서 const diary = useDiary(id)처럼 한 줄로 끝)
    - 로직 캡슐화
    (예외 처리(없는 일기 -> 홈으로 이동 등)를 use Diary에만 정의)
    - 컴포넌트 집중
    (컴포넌트는 UI만 신경쓰고, 로직은 훅에 맡김)
    
    - id에 해당하는 일기를 찾아 반환하는 재사용 가능한 훅
    - DiaryStateContext로부터 전열 일기 목록 접근
    - 일기가 없으면 홈으로 이동시켜 예외 처리까지 포함

---
+++ section12/src/util/get-stringed-date.js
@@ -0,0 +1,17 @@
+// 변환할 데이트 객체
+export const getStringedDate = (targetDate) => {
+  // 날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  // 두자리이하의 달일 때
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
